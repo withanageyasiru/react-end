fda793adc4fca670743d233876764f6d63fc0c37
yuk7hi <yukthi@hotmail.com>
2019-02-12 05:18:56 +0530

    fix changes with temp-presentation

--- src/containers/employees/EmpRequests.js
+++ src/containers/employees/EmpRequests.js
@@ -1,11 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
-import Message from "../../components/EmpRequest/Message";
-import axios from 'axios';
-=======
 import Message from '../../components/EmpRequests/Message';
 
->>>>>>> 16a8d2cae1f96e524736bc647c5d762f4126eaf5
 // EmpRequests - View assets requested by employees
 // For Admins, Department Heads
 class EmpRequests extends React.Component {
