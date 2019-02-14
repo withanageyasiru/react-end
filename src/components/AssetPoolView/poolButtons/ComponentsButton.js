771d0ef53333ed8ece503da2e3c7da17f8817ed2
OvinduWijethunge <42846453+OvinduWijethunge@users.noreply.github.com>
2019-02-15 01:57:59 +0530

    create asset poolrequest button execution

--- src/components/AssetPoolView/poolButtons/ComponentsButton.js
+++ src/components/AssetPoolView/poolButtons/ComponentsButton.js
@@ -9,7 +9,12 @@ class ModalPage extends Component {
       this.state = {
         modal6: false,
         modal7: false,
-        arri:[""]
+        arri:[""],
+        from:"",
+        id:0,
+        to:"",
+        reason:"",
+        description:"",
       }
       this.toggle = this.toggle.bind(this);
     //    console.log(this.props.);
@@ -34,9 +39,11 @@ PutReq=(e)=>{
   //let url = "http://127.0.0.1:8000/api/auth/astreq";
   e.preventDefault();
   Axios.post("http://104.248.24.192:8080/api/auth/astreq", {
-        id : this.props.val.id,
-        type : 1,
-        descript : "my machine is less powerfull "
+    id:this.state.id,
+    from: this.state.from,
+    to: this.state.to,
+    reason: this.state.reason,
+    description: this.state.description
        })
       .then((res) => {
           
@@ -53,6 +60,7 @@ PutReq=(e)=>{
 render() {
   return (
       
+     
     <MDBContainer>
       <MDBBtn color="info" onClick={this.toggle(8)}>More Details</MDBBtn>
       <MDBModal isOpen={this.state.modal8} toggle={this.toggle(8)} fullHeight position="right">
@@ -61,26 +69,79 @@ render() {
         {
         this.state.arri.map(far =>{
             return(
-                <MDBListGroup style={{ width: "22rem" }}key={far.id}>
+              <div>
+              <MDBListGroup style={{ width: "22rem" }}key={far.id}>
                 <MDBListGroupItem>{far.id}</MDBListGroupItem>
                 <MDBListGroupItem>{far.status}</MDBListGroupItem>
                 <MDBListGroupItem>{far.availability}</MDBListGroupItem>
                  <MDBListGroupItem>{far.type}</MDBListGroupItem>
                 <MDBListGroupItem>{"you can choose this asssets ...."}</MDBListGroupItem> 
+               </MDBListGroup>
                
-              </MDBListGroup>
-             
+               <form >
+
+
+ <div className="form-group">
+                        {/* <label className="control-label col-sm-2" htmlFor="id">Asset Id</label> */}
+                        <div className="col-sm-10">
+                        <MDBInput  type="number" label="Asset Id"  name='id'  validate error="wrong"  success="right" onChange={this.handleChange=this.handleChange.bind(this)} />
+     </div>
+</div>                  
+
+
+
+<div className="form-group">
+    {/* <label className="control-label col-sm-2" htmlFor="from">From:</label> */}
+    <div className="col-sm-10">
+    <MDBInput  type="date" label="From"  name='from'  validate error="wrong"  success="right" onChange={this.handleChange=this.handleChange.bind(this)} />
+              </div>
+</div>
+
+<div className="form-group">
+    
+    <div className="col-sm-10">
+    <MDBInput  type="date" label="TO"  name='to'  validate error="wrong"  success="right" onChange={this.handleChange=this.handleChange.bind(this)} />
+              </div>
+</div>
+
+<div className="form-group">
+    
+    <div className="col-sm-10">
+        <select className="form-control" id="reason" type="text" value={this.state.reason} name='reason' onChange={this.handleChange=this.handleChange.bind(this)}>
+
+            <option disabled hidden value=''></option>
+            <option>For Extra Usage</option>
+            <option>For On going Project</option>
+            <option>For Take Home</option>
+            <option>For Getting Out</option>
+        </select>
+    </div>
+
+    <div className="form-group">
+       
+        <div className="col-sm-10">
+            {/* <input type="text" className="form-control" id="description" name="description" placeholder="Any more comments" value={this.state.arequest_description}
+                onChange={this.onChangeDescription}></input> */}
+        <MDBInput  type="text" label="comments"  name='description'  validate error="wrong"  success="right" onChange={this.handleChange=this.handleChange.bind(this)} />
+            
+        </div>
+    </div>
+</div>
+
+</form>
+</div>
             )
         })
           } 
         </MDBModalBody>
         <MDBModalFooter>
           <MDBBtn color="secondary" onClick={this.toggle(8)}>Close</MDBBtn>
-          <MDBBtn color="primary"  onClick={this.PutReq=this.PutReq.bind(this)}>Request This</MDBBtn>
+          <MDBBtn color="primary" onClick={this.PutReq=this.PutReq.bind(this)}>Request This</MDBBtn>
         </MDBModalFooter>
       </MDBModal>
      
     </MDBContainer>
+    
     );
   }
 }
